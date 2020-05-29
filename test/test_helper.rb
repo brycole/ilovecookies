3250caa3772ff3566aacdf6e3f572fcca26cd62b
Bryan Cole <brycole@gmail.com>
2020-05-29 19:19:54 +0100

    updated testing setup and initial test of categories page

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,16 +18,10 @@ class ActiveSupport::TestCase
   #include Devise::Test::IntegrationHelpers
 end
 
-# removed headless operation for initial testing
-# Capybara.register_driver :headless_chrome do |app|
 
-Capybara.register_driver :chrome do |app|
-  #options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu window-size=1400,900])
-  # removed headless options
-  options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox window-size=1400,900])
+Capybara.register_driver :headless_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu window-size=1400,900])
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 Capybara.save_path = Rails.root.join('tmp/capybara')
-Capybara.javascript_driver = :chrome
-
-#Capybara.javascript_driver = :headless_chrome
+Capybara.javascript_driver = :headless_chrome
