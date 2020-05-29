3ffee9d880e94af093c2aaa492b16858add56c18
Bryan Cole <brycole@gmail.com>
2020-05-29 15:37:20 +0100

    seed initial categories and subcategories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# seed file
+
 User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+cookie_categories = %w[Bar Drop Filled Rolled Sandwich]
+
+bar_cat = %w[Chocolate Almond Christmas Lemon]
+drop_cat = %w[Chocolate Almond Cranberry Oatmeal]
+filled_cat = %w[Chocolate Cherry Raisin]
+rolled_cat = %w[Sugar Fruit Ginger Christmas]
+sandwich_cat = %w[Chocolate Ginger Blueberry]
+child_cat = [bar_cat, drop_cat, filled_cat, rolled_cat, sandwich_cat]
+
+# puts child_cat
+# puts child_cat[0]
+
+puts 'Baking initial cookies categories...'
+categories = []
+cookie_categories.each do |name|
+  categories << Category.create!(name: name)
+end
+
+# puts categories
+# puts categories[0]
+# puts categories.class
+
+puts 'Adding filling to each cookie category...'
+
+categories.each_with_index do |category, index|
+  child_cat[index].each do |name|
+    # puts "Adding flavor #{name} to #{category}"
+    name = Category.new(name: name)
+    name.category = category
+    name.save
+  end
+end
