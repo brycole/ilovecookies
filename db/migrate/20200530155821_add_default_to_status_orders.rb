026e8631301844488b1331a388e3b2e32131c227
Bryan Cole <brycole@gmail.com>
2020-05-30 17:00:47 +0100

    add default value of zero to status field in orders table

---
+++ db/migrate/20200530155821_add_default_to_status_orders.rb
@@ -0,0 +1,5 @@
+class AddDefaultToStatusOrders < ActiveRecord::Migration[6.0]
+  def change
+    change_column_default :orders, :status, 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_155247) do
+ActiveRecord::Schema.define(version: 2020_05_30_155821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 2020_05_30_155247) do
 
   create_table "orders", force: :cascade do |t|
     t.bigint "user_id"
-    t.integer "status"
+    t.integer "status", default: 0
     t.float "total_amount"
     t.bigint "billing_address_id"
     t.bigint "delivery_address_id"
