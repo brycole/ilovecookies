5805269b5f9ef6d38cf06d5d2c4fa0e655b60b03
Bryan Cole <brycole@gmail.com>
2020-05-30 16:53:46 +0100

    added rspec testing for order_items. added default quantity of 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_150500) do
+ActiveRecord::Schema.define(version: 2020_05_30_155247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 2020_05_30_150500) do
   end
 
   create_table "order_items", force: :cascade do |t|
-    t.integer "quantity"
+    t.integer "quantity", default: 1
     t.bigint "product_id", null: false
     t.bigint "order_id", null: false
     t.datetime "created_at", precision: 6, null: false
--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe OrderItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "must have a product and an order" do
+    product = Product.create(name: "Cookie")
+    order = Order.create
+
+    order_item = OrderItem.create(product: product, order: order)
+
+    expect(order_item).to be_valid
+  end
+
+  it "cannot have a quantity of zero" do
+    product = Product.create(name: "Cookie")
+    order = Order.create
+
+    order_item = OrderItem.create(product: product, order: order, quantity: 0)
+
+    expect(order_item).to be_invalid
+  end
 end
